bd0c236307de4514f2c0055b022ca3b5fedcd563
Sanskar Pandit <codewithsanskar@gmail.com>
2023-07-18 15:32:41 +0530

    changed to as it was

--- connection.js
+++ connection.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 const dotenv = require("dotenv");
 dotenv.config();
 const mysqlConnection = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  host: "127.0.0.1",
+  user: "root",
+  password: "Samrudhi@123",
+  database: "contactdb",
 });
 mysqlConnection.connect((err) => {
   if (err) {
