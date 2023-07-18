5c2c20490f59567d5eee58af9618091932096dd4
Sanskar Pandit <codewithsanskar@gmail.com>
2023-07-18 20:02:40 +0530

    changes in env file

--- connection.js
+++ connection.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 const dotenv = require("dotenv");
 dotenv.config();
 const mysqlConnection = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "Samrudhi@123",
-  database: "contactdb",
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 });
 mysqlConnection.connect((err) => {
   if (err) {
